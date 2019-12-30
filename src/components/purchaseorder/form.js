9402321ddfc805d22d9f5063c614cb4e6b526d50
Yash Malik <yashmalik23@Yashs-MacBook-Air.local>
2019-12-31 02:14:44 +0530

    PO 50 percent done

--- src/components/purchaseorder/form.js
+++ src/components/purchaseorder/form.js
@@ -3,6 +3,14 @@ import { Text, View, TouchableOpacity, Image, ScrollView, TextInput } from 'reac
 
 import form from './styles/formstyle';
 
+import DropDown from '../helpers/DropDown'
+import SuggestionField from '../helpers/SuggestionField'
+import DatePicker from '../helpers/DatePicker'
+
+
+import truckdata from '../issues/sampledata.json';
+import trailerdata from '../issues/trailerdata.json';
+
 import back from '../../assets/back.png';
 
 class POForm extends React.Component {
@@ -10,9 +18,16 @@ class POForm extends React.Component {
 		super(props);
 		this.state = {
 			editdata: null,
-			formnumber: 1
+            formnumber: 2,
+            equipment:'truck',
+            potype:'general'
 		};
-	}
+    }
+    
+    data = {
+		truck: truckdata,
+		trailer: trailerdata
+	};
 
 	componentDidMount() {
 		this.setState({ editdata: this.props.navigation.getParam('rowdata') });
@@ -30,6 +45,11 @@ class POForm extends React.Component {
 		let ob = { ...this.addeddata };
 		ob[field] = value;
 		this.addeddata = ob;
+    };
+    
+    setEquipment = (field, value) => {
+		this.setValue(field, value);
+		this.setState({ equipment: value.toLowerCase(), override: true });
 	};
 
 	handleSubmit = () => {
@@ -53,6 +73,81 @@ class POForm extends React.Component {
 				</View>
 				<ScrollView style={form.mainform} nestedScrollEnabled={true}>
 					<Text style={form.partnumber}>{this.state.formnumber}/4  </Text>
+                    {this.state.formnumber == 1 && (
+						<React.Fragment>
+							<DropDown
+								label="Equipment Type"
+								name="equipment"
+								value="Select"
+								dropdown={[ 'Truck', 'Trailer' ]}
+								setValue={this.setEquipment}
+							/>
+							<SuggestionField
+								label="Unit number"
+								placeholder="Enter unit number"
+								list={this.data[this.state.equipment]}
+								startSuggestingFrom={1}
+								inputStyle={form.input}
+								suggestBoxStyle={form.suggestbox}
+								setValue={this.setValue}
+								field="id"
+								addfield="unitNo"
+								changeOverRide={() => this.setState({ override: false })}
+								override={this.state.override}
+							/>
+							<Text style={[ form.label, { marginTop: 20 } ]}>Category</Text>
+							<TextInput style={[ form.input, { marginTop: -25 } ]} placeholder="Enter category" />
+							<Text style={[ form.label, { marginTop: 20 } ]}>Subsidiary</Text>
+							<TextInput style={[ form.input, { marginTop: -25 } ]} placeholder="Enter subsidiary" />
+							<DropDown
+								label="Type"
+								name="type"
+								value="Select"
+								dropdown={[ 'General', 'Issue', 'Inventory' ]}
+								setValue={this.setValue}
+							/>
+							<View style={{ height: 10 }} />
+							<DropDown
+								label="Status"
+								name="status"
+								value="Select"
+								dropdown={[ 'Active' ]}
+								setValue={this.setValue}
+							/>
+                            <View style={{ height: 10 }} />
+
+							<Text style={[ form.label, { marginTop: 20 } ]}>Created by</Text>
+							<TextInput style={[ form.input, { marginTop: -25 } ]} placeholder="Enter user name" />
+							<Text style={[ form.label, { marginTop: 20 } ]}>Assigned by</Text>
+							<TextInput style={[ form.input, { marginTop: -25 } ]} placeholder="Enter user name" />
+
+							<View style={{ height: 30 }} />
+						</React.Fragment>
+					)}
+                    {this.state.formnumber == 2 && (
+						<React.Fragment>
+							<Text style={[ form.label, { marginTop: 20 } ]}>Preferred Vendor</Text>
+							<TextInput style={[ form.input, { marginTop: -25 } ]} placeholder="Enter vendor name" />
+                            <DatePicker label="Reported on" value="12-06-2019" name="repOn" setValue={this.setValue} />
+                            <Text style={[ form.label, { marginTop: 20 } ]}>Assigned to</Text>
+							<TextInput style={[ form.input, { marginTop: -25 } ]} placeholder="Enter user name" />
+                            <Text style={[ form.label, { marginTop: 20 } ]}>PO Notes</Text>
+							<TextInput
+								style={[ form.inputarea, { marginTop: -25 } ]}
+								placeholder="Enter notes"
+								multiline={true}
+								numberOfLines={4}
+							/>
+                            <Text style={[ form.label, { marginTop: 0 } ]}>Vendor Notes</Text>
+							<TextInput
+								style={[ form.inputarea, { marginTop: -25 } ]}
+								placeholder="Enter notes"
+								multiline={true}
+								numberOfLines={4}
+							/>
+							<View style={{ height: 30 }} />
+						</React.Fragment>
+					)}
 				</ScrollView>
 				{this.state.formnumber == 4 ? (
 					<Slidebutton submit={this.handleSubmit} />
